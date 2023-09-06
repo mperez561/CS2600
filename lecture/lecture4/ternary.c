3a3c7c8a6b49aa01bcf83acb07b50e4d21ae4e4e
Miguel Perez <maperez1@cpp.edu>
2023-09-05 19:58:47 -0700

    Added ternary lecture notes

---
+++ lecture/lecture4/ternary.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    
+    int a, b;
+
+    a = 10;
+    printf("The value of b is %d\n", (a == 1) ? 20: 30);
+    printf("The value of b is %d\n", (a == 10) ? 20: 30);
+    return 0;
+}
