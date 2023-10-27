990d0f82dbdb330d63481e099efd092df82e9399
Miguel P <maperez1@cpp.edu>
2023-10-26 19:39:22 -0700

    upload 2

---
+++ lecture/mathFunctions/mathFunc.h
@@ -0,0 +1,21 @@
+// Returns a + b 
+
+        extern double Add(double a, double b); 
+
+  
+
+        // Returns a - b 
+
+        extern double Subtract(double a, double b); 
+
+  
+
+        // Returns a * b 
+
+        extern double Multiply(double a, double b); 
+
+  
+
+        // Returns a / b 
+
+        extern double Divide(double a, double b);
